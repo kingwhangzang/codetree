a322b05fef68eb8a5ae11f5bc705f4c80fe69646
kingwhangzang <115606256+kingwhangzang@users.noreply.github.com>
2026-02-18 01:27:36 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 260218/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 260218/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
+
         int[] a = new int[n1];
         int[] b = new int[n2];
-        for (int i = 0; i < n1; i++)
-            a[i] = sc.nextInt();
-        for (int i = 0; i < n2; i++)
-            b[i] = sc.nextInt();
-        
-        boolean Ok = true;
-v:       for(int i = 0; i < n1; i++) {
-            if(b[0] == a[i]) {
-                for(int j = 0; j < n2; j++) {
-                    if(b[j] != a[i+j]) {
-                        Ok = false;
-                        break v;
-                    }
+
+        for (int i = 0; i < n1; i++) a[i] = sc.nextInt();
+        for (int i = 0; i < n2; i++) b[i] = sc.nextInt();
+
+        boolean Ok = false;
+
+        v: for (int i = 0; i <= n1 - n2; i++) {
+            if (a[i] == b[0]) {
+                for (int j = 0; j < n2; j++) {
+                    if (a[i + j] != b[j]) continue v;
                 }
+                Ok = true;
+                break;
             }
         }
-        if(Ok == true) System.out.println("Yes");
-        else System.out.println("No");
+
+        System.out.println(Ok ? "Yes" : "No");
     }
 }
