ee977cd3acf26f5ad66697c5861b271dd1f87770
kingwhangzang <115606256+kingwhangzang@users.noreply.github.com>
2026-02-24 16:09:08 +0900

    Title: 왔다 갔던 구역 2, Time: 214ms, Memory: 14MB, Status: Passed - Codetree

--- 260224/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 260224/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,18 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int res = 0;
-        for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            char dir = sc.next().charAt(0);
+    static class Event{
+        int x;
+        int delta;
+        Event(int x, int delta) {this.x = x; this.delta = delta;}
+    }
 
-            if (dir == 'R') res += x;
-            else if (dir == 'L') res -= x;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        List<Event> events = new ArrayList<>();
+
+        int cur = 0;
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int dist = Integer.parseInt(st.nextToken());
+            char dir = st.nextToken().charAt(0);
+            int next = (dir == 'L') ? cur - dist : cur + dist;
+
+            int a = Math.min(cur,next);
+            int b = Math.max(cur,next);
+
+            events.add(new Event(a, +1));
+            events.add(new Event(b, -1));
+
+             cur = next;
         }
 
-        System.out.println(res);
+        // 이벤트 정렬 (좌표 오름차순)
+        events.sort((e1, e2) -> e1.x - e2.x);
+
+        int ans = 0;
+        int active = 0;
+        int prevX = events.get(0).x;
+
+        int i = 0;
+        while (i < events.size()) {
+            int x = events.get(i).x;
+
+            // prevX ~ x 구간은 active번 겹침
+            if (active >= 2) ans += (x - prevX);
+
+            // 같은 좌표의 이벤트들을 한꺼번에 반영
+            while (i < events.size() && events.get(i).x == x) {
+                active += events.get(i).delta;
+                i++;
+            }
+
+            prevX = x;
+        }
+
+        System.out.println(ans);
     }
 }
